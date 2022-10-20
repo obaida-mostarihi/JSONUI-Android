f20d23bcd64ca588acc84981a4e0962c1710c084
Obaida <obaidaalmostarihi@gmail.com>
2022-10-20 14:37:53 +0300

    Created Models and mappers

---
+++ lib/src/main/java/com/dracode/jsonui/model/PaddingsModel.kt
@@ -0,0 +1,8 @@
+package com.dracode.jsonui.model
+
+internal data class PaddingsModel(
+    val paddingTop: Int?,
+    val paddingBottom: Int?,
+    val paddingRight: Int?,
+    val paddingLeft: Int?,
+)
--- lib/src/main/java/com/dracode/jsonui/view/JsonUiViewExtension.kt
+++ lib/src/main/java/com/dracode/jsonui/view/JsonUiViewExtension.kt
@@ -1,8 +1,15 @@
 package com.dracode.jsonui.view
 
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
+import com.dracode.jsonui.model.JSONViewConfigsMapper
 
-fun View.loadJsonUi(json: String): View{
+fun View.loadJsonUi(rootLayout: ConstraintLayout, json: String?): View{
+    if(json == null) return this
+    val configs = JSONViewConfigsMapper.mapFrom(json)
 
     return this
 }
+
